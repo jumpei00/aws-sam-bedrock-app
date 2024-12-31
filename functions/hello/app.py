3fbd04ff43b60b37554ccdbbe22b40850eb7bdcc
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2024-12-31 17:02:11 +0900

    update lambda function

--- functions/hello/app.py
+++ functions/hello/app.py
@@ -1,42 +1,18 @@
 import json
+from typing import Dict, Any
+from shared.types.models import Hello
 
-# import requests
 
-
-def lambda_handler(event, context):
-    """Sample pure Lambda function
-
-    Parameters
-    ----------
-    event: dict, required
-        API Gateway Lambda Proxy Input Format
-
-        Event doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html#api-gateway-simple-proxy-for-lambda-input-format
-
-    context: object, required
-        Lambda Context runtime methods and attributes
-
-        Context doc: https://docs.aws.amazon.com/lambda/latest/dg/python-context-object.html
-
-    Returns
-    ------
-    API Gateway Lambda Proxy Output Format: dict
-
-        Return doc: https://docs.aws.amazon.com/apigateway/latest/developerguide/set-up-lambda-proxy-integrations.html
-    """
-
-    # try:
-    #     ip = requests.get("http://checkip.amazonaws.com/")
-    # except requests.RequestException as e:
-    #     # Send some context about this error to Lambda Logs
-    #     print(e)
-
-    #     raise e
-
-    return {
-        "statusCode": 200,
-        "body": json.dumps({
-            "message": "hello world",
-            # "location": ip.text.replace("\n", "")
-        }),
-    }
+def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
+    try:
+        response = Hello(message="hello world")
+        return {"statusCode": 200, "body": json.dumps(response.model_dump())}
+    except Exception as e:
+        return {
+            "statusCode": 500,
+            "body": json.dumps(
+                {
+                    "error": json.dumps(str(e)),
+                }
+            ),
+        }
