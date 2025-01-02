f6da035ef54f0feb5444a3781fbea4ed1867f975
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2025-01-02 22:56:49 +0900

    update shared

--- functions/hello/app.py
+++ functions/hello/app.py
@@ -1,11 +1,10 @@
 import json
 from typing import Dict, Any
-from shared.types.models import Hello
 
 
 def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     try:
-        response = Hello(message="hello world!!!!")
+        response = {"message": "hello world"}
         return {"statusCode": 200, "body": json.dumps(response.model_dump())}
     except Exception as e:
         return {
