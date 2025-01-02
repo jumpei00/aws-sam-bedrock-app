47932df3980afc86f14090dd2ca4c8ef7fb4d3fd
Jumpei Motohashi <61613314+jumpei00@users.noreply.github.com>
2025-01-02 23:11:31 +0900

    add response schema

--- functions/hello/app.py
+++ functions/hello/app.py
@@ -1,10 +1,11 @@
 import json
 from typing import Dict, Any
+from shared.types.response import HelloResponse
 
 
 def lambda_handler(event: Dict[str, Any], context: Any) -> Dict[str, Any]:
     try:
-        response = {"message": "hello world"}
+        response = HelloResponse(message="Hello, World!!")
         return {"statusCode": 200, "body": json.dumps(response.model_dump())}
     except Exception as e:
         return {
